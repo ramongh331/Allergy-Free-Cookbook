109757ea4648c7ad6e45fc0e24b25aa8f28175c7
Ramon Hernandez <ramongh331@gmail.com>
2022-12-06 20:33:27 -0500

    added recipe schema

---
+++ models/recipe.js
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////
+//////// Fruits Model
+///////////////////////////////////////////////
+const mongoose = require("./connection");
+
+const { Schema, model } = mongoose;
+
+const recipesSchema = new Schema({
+  name: String,
+  ingredients: String,
+  instructions: String,
+  difficulty: String,
+  allergyFree: {
+    gluten: Boolean,
+    dairy: Boolean,
+    treeNut: Boolean,
+    egg: Boolean,
+    nuts: Boolean,
+    fish: Boolean,
+    shellfish: Boolean,
+    soy: Boolean,
+    sesameSeeds: Boolean,
+    vegan: Boolean,
+    vegetarian: Boolean,
+    keto: Boolean,
+  },
+  //  username: String,
+});
+
+const Recipe = model("Recipe", recipesSchema);
+
+module.exports = Recipe;
